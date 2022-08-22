e8caf137fb168d93384fddfedad7df95d44d2b1a
SmitAkbari <smitakbari20@gnu.ac.in>
2022-08-22 21:25:48 +0530

    Practical1.10

--- src/main/kotlin/practical10.kt
+++ src/main/kotlin/practical10.kt
@@ -1,8 +1,8 @@
 class Car(info:String,Owner:String,miles:Int,OGPrice:Double,currentPrice:Double){
-    lateinit var info:String
+    private lateinit var info:String
     private lateinit var owner:String
     var miles:Int
-    private var OGPrice:Double
+    private var originalPrice:Double
     var currentPrice:Double
 
     init{
@@ -10,14 +10,14 @@ class Car(info:String,Owner:String,miles:Int,OGPrice:Double,currentPrice:Double)
         this.info = info
         this.owner = Owner
         this.miles = miles
-        this.OGPrice = OGPrice
+        this.originalPrice = OGPrice
         this.currentPrice = currentPrice
     }
     private fun getCarInformation(): String {
         return info
     }
     private fun getOriginalCarPrice(): Double {
-        return OGPrice
+        return originalPrice
     }
     private fun getCurrentCarPrice(): Double {
         return currentPrice
@@ -42,9 +42,9 @@ fun main() {
     val car2 = Car("BMW, 2019","Karan",20, 400000.0,399800.0)
     car2.displayCarInfo()
     println("\n********** ArrayList of Car **********")
-    val carlist = ArrayList<Car>()
-    carlist.add(Car("Toyota, 2017","KJS",100,1080000.0,1079000.0))
-    carlist.add(Car("Maruti, 2020","NPP",200,4000000.0,3998000.0))
-    carlist[0].displayCarInfo()
-    carlist[1].displayCarInfo()
+    val carList = ArrayList<Car>()
+    carList.add(Car("Toyota, 2017","KJS",100,1080000.0,1079000.0))
+    carList.add(Car("Maruti, 2020","NPP",200,4000000.0,3998000.0))
+    carList[0].displayCarInfo()
+    carList[1].displayCarInfo()
 }
