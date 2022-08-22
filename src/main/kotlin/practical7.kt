6ead44b4a790ef1b505ca4ebc60e72d91c08f83b
SmitAkbari <smitakbari20@gnu.ac.in>
2022-08-22 20:34:49 +0530

    Practical1.7

---
+++ src/main/kotlin/practical7.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val num = 5
+    println("Factorial of $num = ${findFactorial(num)}")
+    println("By TailRec Keyword, Factorial of $num = ${factorial(num)}")
+
+}
+fun findFactorial(num:Int):Long{
+    if(num==1){
+        return 1
+    }
+    while(num>0){
+        return num*findFactorial(num-1)
+    }
+
+    return -1
+}
+tailrec fun factorial(num: Int, run: Int = 1): Long {
+    return if (num == 1){
+        run.toLong()
+    } else {
+        factorial(num-1, run*num)
+    }
+}
